8ad56cad81e0e20dc69a55c28bd6447e2ff6ceed
takuya <takuya3211@gmail.com>
2018-04-03 15:23:31 +0900

    teset
    test

--- getCorrection.py
+++ getCorrection.py
@@ -1,25 +1,20 @@
+# coding: UTF-8
 import os
 import glob
 import csv
-# rename MedicalRecordNumber to serial number using Taioh.txt
 # git_test2
 
+#  keyword = "./*.INI.XVI"
 keyword = "./PatB/*.INI.XVI"
-#print(keyword)
 files=glob.glob(keyword)
-#files = './MorningPenta/1.3.46.423632.33592720171226235853625.1.27.12.201785854015.INI.XVI'
+
 writefile = open('./test.csv', 'w')
 writefile.write("Date, Time, approver, x, y, z, x-rot, y-rot, z-rot \n")
-#print(len(files[0]))
-#print(len(files[1]))
-#print(files[0])
-#print(files[1])
+
+#ここ後で改良
 filenamelength = 65 #very poor
 for file in files:
-	print(file)
-	print (len(file))
 	if len(file) > filenamelength:
-		#print(len(file))
 		textlist = []
 		for line in open(file,'r'):
 			line = line.replace('\n','')
@@ -41,16 +36,13 @@ for file in files:
 				#print line
 			if 'Align.correction' in line:
 				correction = line.replace('Align.correction=','')
-			######poor code ! fix it ! from here ########
-				#correction.append(line)
-				#print line
+				
+			#359.0degを-1.0degに変更
 				splitCorrection = correction.split(",")
-				#print splitCorrection[3]
 				for i in range(3):
 					if float(splitCorrection[i + 3]) > 350.0:
 						splitCorrection[i+3] = float(splitCorrection[i + 3]) - 360
 				correction = splitCorrection[0] + "," + splitCorrection[1] + "," + splitCorrection[2] + "," + str(splitCorrection[3]) + "," + str(splitCorrection[4]) + "," +  str(splitCorrection[5])
-			######poor code ! fix it !  to here ########
 					
 		text_tuple = (date+ ',', time+ ',', approvalby+ ',', correction)
 		text = "".join(map(str,text_tuple))
@@ -60,4 +52,4 @@ for file in files:
 		for x in textlist: 
 			if len(file) > filenamelength:
 				writefile.write(str(textlist[0]) + '\n')
-	print("Finished!!")
+print("Finished!!")
