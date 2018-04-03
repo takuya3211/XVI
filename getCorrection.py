d165daba3f9986de3aa44062be43c22ba357a031
takuya <takuya3211@gmail.com>
2018-04-03 16:42:05 +0900

    ref only
    [REFFERENCE] only

--- getCorrection.py
+++ getCorrection.py
@@ -12,13 +12,25 @@ writefile = open('./test.csv', 'w')
 writefile.write("Date, Time, approver, x, y, z, x-rot, y-rot, z-rot \n")
 
 #ここ後で改良
-filenamelength = 65 #very poor
+#[REFERENCE]がほしいタグ
+filenamelength = 0 #very poor
+
 for file in files:
 	if len(file) > filenamelength:
 		textlist = []
 		for line in open(file,'r'):
 			line = line.replace('\n','')
 			line = line.replace('\r','')
+			if line == "[RECONSTRUCTION]":
+				print('this is reconfile')
+				flag = 1
+				break
+			else if line == "[STATUS]":
+				flag = 1
+				break
+			else
+				flag = 0
+				
 			if 'AlignmentApprovalDate' in line:
 				date = line.replace('AlignmentApprovalDate=','')
 				excelDate=date[0:4] + "/" + date[4:6] + "/" + date[6:8]
@@ -43,7 +55,9 @@ for file in files:
 					if float(splitCorrection[i + 3]) > 350.0:
 						splitCorrection[i+3] = float(splitCorrection[i + 3]) - 360
 				correction = splitCorrection[0] + "," + splitCorrection[1] + "," + splitCorrection[2] + "," + str(splitCorrection[3]) + "," + str(splitCorrection[4]) + "," +  str(splitCorrection[5])
-					
+		
+		if flag == 1:
+			continue			
 		text_tuple = (date+ ',', time+ ',', approvalby+ ',', correction)
 		text = "".join(map(str,text_tuple))
 		textlist.append(text)
